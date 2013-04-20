af862437a9c9b4964722c8d6a75fa2769db7b95b
knay <payen.john12@gmail.com>
2013-04-20 19:28:09 +0200

    commit du R.java

--- gen/killheart/knay/trombinoscope/R.java
+++ gen/killheart/knay/trombinoscope/R.java
@@ -10,29 +10,55 @@ package killheart.knay.trombinoscope;
 public final class R {
     public static final class attr {
     }
+    public static final class dimen {
+        /**  Default screen margins, per the Android Design guidelines. 
+
+         Customize dimensions originally defined in res/values/dimens.xml (such as
+         screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
+    
+         */
+        public static final int activity_horizontal_margin=0x7f040000;
+        public static final int activity_vertical_margin=0x7f040001;
+    }
     public static final class drawable {
         public static final int ic_action_search=0x7f020000;
         public static final int ic_launcher=0x7f020001;
+        public static final int iconephoto=0x7f020002;
+        public static final int limite_tete_photo=0x7f020003;
     }
     public static final class id {
-        public static final int menu_settings=0x7f070000;
+        public static final int action_settings=0x7f080005;
+        public static final int boutonPhoto=0x7f080001;
+        public static final int listelayout=0x7f080002;
+        public static final int menu_settings=0x7f080004;
+        public static final int surfaceViewCamera=0x7f080000;
+        public static final int trombilayout=0x7f080003;
     }
     public static final class layout {
         public static final int activity_camera=0x7f030000;
-        public static final int activity_main=0x7f030001;
+        public static final int activity_liste=0x7f030001;
+        public static final int activity_main=0x7f030002;
+        public static final int activity_trombi=0x7f030003;
     }
     public static final class menu {
-        public static final int activity_camera=0x7f060000;
-        public static final int activity_main=0x7f060001;
+        public static final int activity_camera=0x7f070000;
+        public static final int activity_main=0x7f070001;
+        public static final int liste=0x7f070002;
+        public static final int trombi=0x7f070003;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_camera=0x7f040004;
-        public static final int title_activity_main=0x7f040003;
+        public static final int action_settings=0x7f050008;
+        public static final int app_name=0x7f050000;
+        public static final int hello_world=0x7f050001;
+        public static final int limite_tete_str=0x7f050005;
+        public static final int menu_settings=0x7f050002;
+        public static final int prendre_Photo_str=0x7f050006;
+        public static final int title_activity_camera=0x7f050004;
+        public static final int title_activity_liste=0x7f050007;
+        public static final int title_activity_main=0x7f050003;
+        public static final int title_activity_trombi=0x7f050009;
     }
     public static final class style {
-        public static final int AppTheme=0x7f050000;
+        public static final int AppTheme=0x7f060000;
     }
 }

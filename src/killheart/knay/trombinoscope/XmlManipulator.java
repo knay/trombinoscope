7b2a726fd8038740abf9dd4219bc870716701776
david <david@localhost>
2013-04-22 19:41:39 +0200

    dd

--- src/killheart/knay/trombinoscope/XmlManipulator.java
+++ src/killheart/knay/trombinoscope/XmlManipulator.java
@@ -3,6 +3,7 @@ package killheart.knay.trombinoscope;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Iterator;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -10,10 +11,12 @@ import javax.xml.parsers.ParserConfigurationException;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
 public class XmlManipulator {
-
+	private Element rootXml;
 	/**
 	 * @author David et Jonathan
 	 * 
@@ -54,12 +57,30 @@ public class XmlManipulator {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		Element root = dom.getDocumentElement();//< Racine de l'arbre du document, et fournit l'accès principal aux données du document.
+		rootXml = dom.getDocumentElement();//< Racine de l'arbre du document, et fournit l'accès principal aux données du document.
 			
 	}
-	public void Lire(){
+	
+	/**
+	 * @author David et Jonathan
+	 * 
+	 * Fonction Lire permet d'accéder à la baliser passé
+	 * en parametre.
+	 *  
+	 *  @param tags balise qu'on accéder
+	 */
+	public void Lire(String tags){
+		Pupil e =new Pupil();
+		Grade scolaire = new Grade();
 		
-		
+		NodeList items = rootXml.getElementsByTagName(tags);//< On crée une List contenant tous les noeuds "tags"
+		for(int i=0; i<items.getLength(); i++){
+				String nom = items.item(0).getNodeValue();
+				e.setNom(nom);
+				String prenom = items.item(1).getNodeValue();
+				e.setPrenom(prenom);
+	   	}
+		scolaire.ajouterEleve(e);
 		
 	}
 	

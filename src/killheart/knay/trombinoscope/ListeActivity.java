94ad93e07d21071fec85cfe674e4f11ba50d7f75
knay <payen.john12@gmail.com>
2013-05-03 22:38:02 +0200

    dsdq

--- src/killheart/knay/trombinoscope/ListeActivity.java
+++ src/killheart/knay/trombinoscope/ListeActivity.java
@@ -7,7 +7,6 @@ import android.os.Environment;
 import android.app.Activity;
 import android.content.Intent;
 import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
